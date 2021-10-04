a09e8f54e42990135fac7bea7be63bb4e7acef49
Tramy Nguyen <tramynn2@gmail.com>
2021-10-04 18:57:25 -0500

    Removed contractOwner for fallback fn; Adds transfer to sender;

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -46,7 +46,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function contractOwner() external payable {
+    function () external payable {
         revert();
     }
 
@@ -97,7 +97,9 @@ contract SimpleBank {
       if ( balances[ msg.sender ] >= withdrawAmount ) {
       // Subtract the amount from the sender's balance, and try to send that amount of ether
       // to the user attempting to withdraw. 
-      // 2. Transfer Eth to the sender and decrement the withdrawal amount from
+      // 2. Transfer Eth to the sender
+        msg.sender.transfer(withdrawAmount);
+      // and decrement the withdrawal amount from
       //    sender's balance
         balances[ msg.sender ] -= withdrawAmount;
       }
